5f7cde68011d81582c80a236b7b7867ce002615e
xunicatt <aniketbiswas625@gmail.com>
2024-12-12 20:28:55 +0530

    fixed '*bit.h' header file issue

--- README.md
+++ README.md
@@ -1,20 +1,25 @@
 # TFT_eSPI_Scroll
+
 TFT_eSPI_Scroll library adds text scroll functionality to the existing [TFT_eSPI](https://github.com/Bodmer/TFT_eSPI/) library. This library by default uses 1-bit color-depth which makes it perform very fast & flickerless with very minimum ram usages. The color-depth can be changed accordingly to match required performances. This library supports all the processors that [TFT_eSPI](https://github.com/Bodmer/TFT_eSPI/) library supports. Available in [PlatformIO Registry](https://registry.platformio.org/libraries/xunicatt/TFT_eSPI_Scroll) and [Arduino Library Manager](https://www.arduino.cc/reference/en/libraries/tft_espi_scroll/).
 
 # Installation
 
 ### Platform IO:
+
 1. Open `platformio.ini`, a project configuration file located in the root of PlatformIO project.
 2. Add the following line to the lib_deps option of [env:] section:
+
 ```
 xunicatt/TFT_eSPI_Scroll@^1.0.1
 ```
+
 3. Build a project, PlatformIO will automatically install dependencies.
 
 ### Arduino:
+
 1. Open the Arduino IDE.
 2. With the editor open, let's take a look at the left column. Here, we can see a couple of icons. Let's click the on the "library" icon.
-3. A list will now appear of all available libraries, search for `TFT_eSPI_Scroll`. 
+3. A list will now appear of all available libraries, search for `TFT_eSPI_Scroll`.
 4. Select a version and Click Install.
 
 # Include
@@ -28,6 +33,7 @@ TFT_eSPI_Scroll library provides the following header files that can be included
 # Usage
 
 ### Object Creation:-
+
 ```cpp
 #include <TFT_eSPI.h>
 #include <TFT_eSPI_Scroll.h>
@@ -38,8 +44,8 @@ TFT_eSPI_Scroll scroll;
 
 ### Initialization:-
 
->[!WARNING]
->Note it is important to initialize the "TFT_eSPI" library before using "TFT_eSPI_Scroll".
+> [!WARNING]
+> Note it is important to initialize the "TFT_eSPI" library before using "TFT_eSPI_Scroll".
 
 Using this method the "TFT_eSPI_Scroll" library initializes in 1-bit color-depth mode. Only black and white color is available in this color-depth. Though 1-bit color-depth is limited to B/W colors, it is not only fast but also memory efficient and renders without any flickering or glitches.
 
@@ -56,7 +62,8 @@ void setup(){
 }
 ```
 
-To set other color-depths, intialize the library with: 
+To set other color-depths, intialize the library with:
+
 ```cpp
 // previous codes
 
@@ -68,7 +75,6 @@ if(scroll.init(&tft, textFont, colorDepth) != NO_ERROR){
 
 // rest of the code
 ```
-
 
 ### Writing to display:-
 
@@ -82,8 +88,8 @@ void loop(){
 
 ### Changing the background & foreground color:-
 
->[!WARNING]
->Note in order to apply proper colors (excluding 1-Bit default), proper color-depth must be selected during initialization.
+> [!WARNING]
+> Note in order to apply proper colors (excluding 1-Bit default), proper color-depth must be selected during initialization.
 
 ```cpp
 // bg & fg {8-bit}
@@ -103,4 +109,5 @@ scroll.reset();
 ```
 
 # Version
->1.0.4
+
+> 1.0.5
--- src/TFT_eSPI_Scroll.h
+++ src/TFT_eSPI_Scroll.h
@@ -1,8 +1,9 @@
 /*
 **  Author: Aniket Biswas
 **  Github: https://github.com/xunicatt
-**  
-**  Library to add Vertical text Scrolling functionality to TFT_eSPI library by bodmer.
+**
+**  Library to add Vertical text Scrolling functionality to TFT_eSPI library by
+*bodmer.
 **  TFT_eSPI: https://github.com/Bodmer/TFT_eSPI
 **
 **  Contribute to improve the library
@@ -19,80 +20,78 @@
 #include <TFT_eSPI.h>
 #include <vector>
 
-#include "./1bit.h"
-#include "./4bit.h"
-
-#define TFT_eSPI_Scroll_Version "1.0.4"
+#define TFT_eSPI_Scroll_Version "1.0.5"
 
 /*
 **  Error Handeling
 **  Returns NO_ERROR if everthing is ok
 */
-enum TFT_eSPI_Scroll_Error{
-    NO_ERROR,
-    ERROR_NO_TFT,
-    ERROR_SPRITE_MEMORY_ALLOCATION_FAILED
+enum TFT_eSPI_Scroll_Error {
+  NO_ERROR,
+  ERROR_NO_TFT,
+  ERROR_SPRITE_MEMORY_ALLOCATION_FAILED
 };
-
 
 /*
 ** TFT_eSPI_Scroll
 ** Enables text scrolling
 */
 
-class TFT_eSPI_Scroll{
+class TFT_eSPI_Scroll {
 public:
+  // Constructor
+  TFT_eSPI_Scroll();
+  // Destructor
+  ~TFT_eSPI_Scroll();
 
-    // Constructor
-    TFT_eSPI_Scroll();
-    // Destructor
-    ~TFT_eSPI_Scroll();
+  /*
+  ** @name init
+  ** @brief Initializes the class with neccessary datas.
+  ** @brief It is recommended to use default 1-bit color_depth [B/W] for
+  *flickerless screen refresh & low ram usage
+  ** @param TFT_eSPI*
+  ** @param text_font
+  ** @param color_depth
+  */
+  TFT_eSPI_Scroll_Error init(TFT_eSPI *tft_espi, uint16_t text_font,
+                             uint8_t color_depth = 1);
 
-    /*
-    ** @name init
-    ** @brief Initializes the class with neccessary datas.
-    ** @brief It is recommended to use default 1-bit color_depth [B/W] for flickerless screen refresh & low ram usage
-    ** @param TFT_eSPI*
-    ** @param text_font
-    ** @param color_depth
-    */
-    TFT_eSPI_Scroll_Error init(TFT_eSPI *tft_espi, uint16_t text_font, uint8_t color_depth = 1);
-    
-    /*
-    ** @name write
-    ** @brief Writes a line to the screen while scrolling.
-    ** @param text
-    */
-    TFT_eSPI_Scroll_Error write(String text);
+  /*
+  ** @name write
+  ** @brief Writes a line to the screen while scrolling.
+  ** @param text
+  */
+  TFT_eSPI_Scroll_Error write(String text);
 
-    /*
-    ** @name setColor
-    ** @brief Sets an background and foreground color
-    ** @brief It is recommended to use default 1-bit color settings [B/W] for flickerless screen refresh & low ram usage.
-    ** @brief Color depth must be set accordingly.
-    ** @param background_color
-    ** @param foreground_color
-    */
-    void setColor(uint16_t background_color, uint16_t forground_color);
+  /*
+  ** @name setColor
+  ** @brief Sets an background and foreground color
+  ** @brief It is recommended to use default 1-bit color settings [B/W] for
+  *flickerless screen refresh & low ram usage.
+  ** @brief Color depth must be set accordingly.
+  ** @param background_color
+  ** @param foreground_color
+  */
+  void setColor(uint16_t background_color, uint16_t forground_color);
 
-    /*
-    ** @name reset
-    ** @brief Resets configs, init must be called 
-    */ 
-    void reset();
+  /*
+  ** @name reset
+  ** @brief Resets configs, init must be called
+  */
+  void reset();
 
 private:
-    // Scrolls the screen by shifting all lines by -1
-    void shift(int text_width);
+  // Scrolls the screen by shifting all lines by -1
+  void shift(int text_width);
 
-    int width, height; //screen size
-    int font_height, cursor;
+  int width, height; // screen size
+  int font_height, cursor;
 
-    //stroing the lines in display 
-    std::vector<String> buffer;
-    TFT_eSPI *tft; //tft_espi display driver
-    TFT_eSprite *sprite; //sprite for flicker less update
-    uint16_t bg_color;
+  // stroing the lines in display
+  std::vector<String> buffer;
+  TFT_eSPI *tft;       // tft_espi display driver
+  TFT_eSprite *sprite; // sprite for flicker less update
+  uint16_t bg_color;
 };
 
 #endif
